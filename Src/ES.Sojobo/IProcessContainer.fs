2f648b423ca7c0855c26652e7dc3ecfa984a0455
Antonio Parata <aparata@gmail.com>
2019-04-30 22:15:54 +0200

    Created interfaces

--- Src/ES.Sojobo/IProcessContainer.fs
+++ Src/ES.Sojobo/IProcessContainer.fs
@@ -3,17 +3,22 @@
 open System
 open ES.Sojobo.Model
 open B2R2.BinFile
+open B2R2.FrontEnd
 
 type IProcessContainer =
     interface 
         abstract GetProgramCounter: unit -> EmulatedValue
         abstract GetProgramCounterValue: unit -> UInt64
         abstract GetArgument: position: Int32 -> EmulatedValue
+        abstract SetVariable: EmulatedValue -> unit
+        abstract GetVariable: name: String -> EmulatedValue
         abstract ReadMemory: address: UInt64 * size: Int32 -> Byte array
         abstract WriteMemory: UInt64 * Byte array -> unit
         abstract UpdateMemoryRegion: MemoryRegion * MemoryRegion -> unit
         abstract GetActiveMemoryRegion: unit -> MemoryRegion
         abstract GetMemoryRegion: UInt64 -> MemoryRegion
         abstract GetImportedFunctions: unit -> Symbol seq
+        abstract GetInstruction: unit -> Instruction
+        abstract Step : IEvent<IProcessContainer> with get
     end
 
--- Src/ES.Sojobo/ISandbox.fs
+++ Src/ES.Sojobo/ISandbox.fs
@@ -4,8 +4,10 @@ open System
 
 type ISandbox =
     interface 
-        abstract Run: String -> unit
-        abstract Run: Byte array -> unit
-        abstract AddCallback: functionName: String * moduleName: String * callback: Action<IProcessContainer> -> unit
+        abstract Create: String -> unit
+        abstract Create: Byte array -> unit  
+        abstract Run: unit -> unit
+        abstract Stop: unit -> unit     
+        abstract GetRunningProcess: unit -> IProcessContainer
     end
 
