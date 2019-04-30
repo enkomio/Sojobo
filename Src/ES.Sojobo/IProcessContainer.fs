04f3c763e07f43fa1b6750a03c6f463749097f7d
Antonio Parata <aparata@gmail.com>
2019-04-30 15:08:27 +0200

    Created interface

---
+++ Src/ES.Sojobo/IProcessContainer.fs
@@ -0,0 +1,19 @@
+﻿namespace ES.Sojobo
+
+open System
+open ES.Sojobo.Model
+open B2R2.BinFile
+
+type IProcessContainer =
+    interface 
+        abstract GetProgramCounter: unit -> EmulatedValue
+        abstract GetProgramCounterValue: unit -> UInt64
+        abstract GetArgument: position: Int32 -> EmulatedValue
+        abstract ReadMemory: address: UInt64 * size: Int32 -> Byte array
+        abstract WriteMemory: UInt64 * Byte array -> unit
+        abstract UpdateMemoryRegion: MemoryRegion * MemoryRegion -> unit
+        abstract GetActiveMemoryRegion: unit -> MemoryRegion
+        abstract GetMemoryRegion: UInt64 -> MemoryRegion
+        abstract GetImportedFunctions: unit -> Symbol seq
+    end
+
---
+++ Src/ES.Sojobo/ISandbox.fs
@@ -0,0 +1,11 @@
+﻿namespace ES.Sojobo
+
+open System
+
+type ISandbox =
+    interface 
+        abstract Run: String -> unit
+        abstract Run: Byte array -> unit
+        abstract AddCallback: functionName: String * moduleName: String * callback: Action<IProcessContainer> -> unit
+    end
+
