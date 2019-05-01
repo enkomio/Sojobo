c701621212094fbb20917489ad74465baae8530c
Antonio Parata <aparata@gmail.com>
2019-05-01 10:44:29 +0200

    Inherit from base class

--- Src/ES.Sojobo/Win32Sandbox.fs
+++ Src/ES.Sojobo/Win32Sandbox.fs
@@ -1,117 +1,134 @@
 ﻿namespace ES.Sojobo
 
 open System
+open System.Reflection
 open System.Collections.Generic
-open B2R2.BinFile
 open B2R2.FrontEnd
 open B2R2
-open B2R2.BinIR
-open B2R2.BinIR.LowUIR
+open ES.Sojobo.Model
 
-module Win32Sandbox =
-    type SandboxSettings = {
-        PrintAssembly: Boolean
-        PrintIR: Boolean
-    }
+type Win32Sandbox() =
+    let _assemblies = new List<Assembly>()
+    let _libraryFunctions = new Dictionary<String, MethodInfo>()
+    let _callbacks = new Dictionary<UInt64, String>()       
+    let mutable _stopExecution = false
+    let mutable _currentProcess: Win32ProcessContainer option = None
 
-    let defaultSandboxConfig = {
-        PrintAssembly = false
-        PrintIR = false
-    }
+    let getFunctionKeyName(functioName: String, libraryName: String) =
+        let keyName = String.Format("{0}::{1}", libraryName, functioName).ToLower()
+        keyName.Replace(".dll", String.Empty)
 
-    type Win32Sandbox(settings: SandboxSettings) =
-        let _callbacks = new Dictionary<UInt64, String>()
-        let _emulatedCallbacks = new Dictionary<String, IProcessContainer -> unit>()
+    let mapImportedFunctions(win32Process: Win32ProcessContainer) =
+        win32Process.GetImportedFunctions()
+        |> Seq.iter(fun symbol ->
+            let keyName = getFunctionKeyName(symbol.Name, symbol.LibraryName)
+            if _libraryFunctions.ContainsKey(keyName) then
+                _callbacks.[symbol.Address] <- keyName
+                let addressBytes = uint32 symbol.Address |> BitConverter.GetBytes
+                win32Process.WriteMemory(symbol.Address, addressBytes)
+        )
 
-        let prinAssembly(handler: BinHandler, instruction: Instruction) =
-            if settings.PrintAssembly then
-                let disassembledInstruction = BinHandler.DisasmInstr handler false true instruction 
-                let instructionBytes = BinHandler.ReadBytes(handler , instruction.Address, int32 instruction.Length)                
-                let hexBytes = BitConverter.ToString(instructionBytes).Replace("-"," ")
-                let text = String.Format("0x{0,-10} {1, -30} {2}", instruction.Address.ToString("X") + ":", hexBytes, disassembledInstruction)
-                Console.WriteLine(text)
+    let emulateInstruction(handler: BinHandler, instruction: Instruction, baseProcess: BaseProcessContainer) =
+        BinHandler.LiftInstr handler instruction
+        |> Array.iter(LowUIREmulator.emulateStmt baseProcess)
 
-        let printIR(statement: Stmt) =
-            if settings.PrintIR then
-                let statementString = LowUIR.Pp.stmtToString statement
-                Console.WriteLine(statementString)
+    let executeReturn =
+        let arrayBuffer = Array.zeroCreate<Byte>(1)
+        arrayBuffer.[0] <- 0xC3uy
+        (*
+        // TODO:
+        if I have to clean the stack is: C2 0400                  | ret 4 
+        *)
 
-        let getFunctionKeyName(functioName: String, libraryName: String) =
-            let keyName = String.Format("{0}::{1}", libraryName, functioName).ToLower()
-            keyName.Replace(".dll", String.Empty)
+        let handler = BinHandler.Init(ISA.OfString "x86", ArchOperationMode.NoMode, true, Addr.MinValue, arrayBuffer)
+        let retInstruction = BinHandler.ParseInstr handler Addr.MinValue
+        fun (baseProcess: BaseProcessContainer) -> 
+            let handler = baseProcess.GetActiveMemoryRegion().Handler
+            emulateInstruction(handler, retInstruction, baseProcess)
 
-        let mapImportedFunctions(win32Process: Win32ProcessContainer) =
-            win32Process.GetImportedFunctions()
-            |> Seq.iter(fun symbol ->
-                let keyName = getFunctionKeyName(symbol.Name, symbol.LibraryName)
-                if _emulatedCallbacks.ContainsKey(keyName) then
-                    _callbacks.[symbol.Address] <- keyName
-                    let addressBytes = uint32 symbol.Address |> BitConverter.GetBytes
-                    win32Process.WriteMemory(symbol.Address, addressBytes)
-            )
+    let resolveLibraryFunctions(assemblies: Assembly seq) =
+        assemblies
+        |> Seq.collect(fun assembly -> assembly.GetTypes())
+        |> Seq.collect(fun t -> t.GetMethods())
+        |> Seq.filter(fun m -> m.IsStatic && m.ReturnType = typeof<CallbackResult>)
+        |> Seq.filter(fun m -> m.GetParameters().Length > 0 && m.GetParameters().[0].ParameterType = typeof<IProcessContainer>)
+        |> Seq.filter(fun m ->
+            let parameters = m.GetParameters()
+            if parameters.Length > 1 then
+                parameters
+                |> Array.skip 1
+                |> Array.forall(fun p -> 
+                    // must be all integers
+                    [typeof<Int32>; typeof<UInt32>]
+                    |> List.contains(p.ParameterType)
+                )
+            else
+                true
+        )
+        |> Seq.iter(fun m ->
+            let keyName = getFunctionKeyName(m.Name, m.DeclaringType.Name)
+            _libraryFunctions.[keyName] <- m
+        )
 
-        let emulateInstruction(handler: BinHandler, instruction: Instruction, win32Process: Win32ProcessContainer) =
-            BinHandler.LiftInstr handler instruction
-            |> Array.iter(fun statement ->
-                printIR(statement)
-                LowUIREmulator.emulateStmt win32Process statement
-            )
+    let invokeLibraryFunction(baseProcess: BaseProcessContainer) =
+        let keyName = _callbacks.[baseProcess.GetProgramCounterValue()]
+        let libraryFunction = _libraryFunctions.[keyName]
+        let libraryFunctionResult = libraryFunction.Invoke(null, [|baseProcess|])
+        executeReturn(baseProcess)
 
-        let executeReturn =
-            let arrayBuffer = Array.zeroCreate<Byte>(1)
-            arrayBuffer.[0] <- 0xC3uy
-            let handler = BinHandler.Init(ISA.OfString "x86", ArchOperationMode.NoMode, true, Addr.MinValue, arrayBuffer)
-            let retInstruction = BinHandler.ParseInstr handler Addr.MinValue
-            fun (win32Process: Win32ProcessContainer) -> 
-                let handler = win32Process.GetActiveMemoryRegion().Handler
-                emulateInstruction(handler, retInstruction, win32Process)
+    member this.AddLibrary(assembly: Assembly) =
+        _assemblies.Add(assembly)
 
-        let runProcess(win32Process: Win32ProcessContainer) =
-            mapImportedFunctions(win32Process)            
-            let activeRegion = win32Process.GetActiveMemoryRegion()
-            let endAddress = activeRegion.BaseAddress + uint64 activeRegion.Content.Length
-            let mutable completed = false
-
-            while not completed do
-                // check if called an emulated function
-                if win32Process.GetProgramCounterValue() |> _callbacks.ContainsKey then
-                    let keyName = _callbacks.[win32Process.GetProgramCounterValue()]
-                    let callback = _emulatedCallbacks.[keyName]
-                    callback(win32Process)
-                    executeReturn(win32Process)
-                else
-                    let instruction = win32Process.GetNextInstruction()
-                    completed <- instruction.Address + uint64 instruction.Length >= endAddress
-
-                    let handler = win32Process.GetActiveMemoryRegion().Handler
-                    prinAssembly(handler, instruction)
-                                
-                    // emulate instruction
-                    emulateInstruction(handler, instruction, win32Process)
-
-        // run with default settings
-        new() = new Win32Sandbox(defaultSandboxConfig)
-
-        member this.AddCallback(functionName: String, moduleName: String, callback: Action<IProcessContainer>) =
-            let keyName = getFunctionKeyName(functionName, moduleName)
-            _emulatedCallbacks.[keyName] <- FuncConvert.FromAction(callback)
+    member this.Run() =            
+        let win32Process = _currentProcess.Value
+        let activeRegion = win32Process.GetActiveMemoryRegion()
+        let endAddress = activeRegion.BaseAddress + uint64 activeRegion.Content.Length
         
-        member this.Run(filename: String) =
-            let win32Process = new Win32ProcessContainer()
-            win32Process.Initialize(filename)
-            runProcess(win32Process)
+        // prepare for execution
+        resolveLibraryFunctions([Assembly.GetExecutingAssembly()])
+        resolveLibraryFunctions(_assemblies)
+        mapImportedFunctions(win32Process)
+                        
+        // start execution loop
+        _stopExecution <- false
+        while not _stopExecution do
+            // check if called an emulated function
+            if win32Process.GetProgramCounterValue() |> _callbacks.ContainsKey then
+                invokeLibraryFunction(win32Process)
+            else                    
+                let instruction = win32Process.ReadNextInstruction()
+                _stopExecution <- _stopExecution || instruction.Address + uint64 instruction.Length >= endAddress
 
-        member this.Run(buffer: Byte array) =
-            let win32Process = new Win32ProcessContainer()
-            win32Process.Initialize(buffer)
-            runProcess(win32Process)
+                // emulate instruction
+                let handler = win32Process.GetActiveMemoryRegion().Handler                    
+                emulateInstruction(handler, instruction, win32Process)
 
-        interface  ISandbox with
-            member this.AddCallback(functionName: String, moduleName: String, callback: Action<IProcessContainer>) =
-                this.AddCallback(functionName, moduleName, callback)
+    member this.Stop() =
+        _stopExecution <- true
         
-            member this.Run(filename: String) =
-                this.Run(filename)
+    member this.Create(filename: String) =
+        _currentProcess <- new Win32ProcessContainer() |> Some
+        _currentProcess.Value.Initialize(filename)
 
-            member this.Run(buffer: Byte array) =
-                this.Run(buffer)
+    member this.Create(buffer: Byte array) =
+        _currentProcess <- new Win32ProcessContainer() |> Some
+        _currentProcess.Value.Initialize(buffer)
+
+    member this.GetRunningProcess() =
+        _currentProcess.Value :> IProcessContainer
+
+    interface  ISandbox with
+        member this.Create(filename: String) =
+            this.Create(filename)
+
+        member this.Create(buffer: Byte array) =
+            this.Create(buffer)
+
+        member this.Run() =
+            this.Run()
+
+        member this.Stop() =
+            this.Stop()
+            
+        member this.GetRunningProcess() =
+            this.GetRunningProcess()
