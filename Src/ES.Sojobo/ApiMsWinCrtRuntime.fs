733116eff7ae30ebbb9426ddecd32eb6aa100edd
Antonio Parata <aparata@gmail.com>
2019-05-10 00:34:24 +0200

    Added more functions templates

--- Src/ES.Sojobo/ApiMsWinCrtRuntime.fs
+++ Src/ES.Sojobo/ApiMsWinCrtRuntime.fs
@@ -11,3 +11,28 @@ module ``api-ms-win-crt-runtime-l1-1-0`` =
             ReturnValue = None
             Convention = CallingConvention.Cdecl
         }
+
+    let _initterm_e(sandbox: ISandbox, arg0: UInt32, arg1: UInt32) = {
+        ReturnValue = Some <| createInt32(0).Value
+        Convention = CallingConvention.Cdecl
+    }
+
+    let _initterm(sandbox: ISandbox, arg0: UInt32, arg1: UInt32) = {
+        ReturnValue = None
+        Convention = CallingConvention.Cdecl
+    }
+    
+    let __p___argv(sandbox: ISandbox) = {
+        ReturnValue = None
+        Convention = CallingConvention.Cdecl
+    }
+
+    let __p___argc(sandbox: ISandbox) = {
+        ReturnValue = None
+        Convention = CallingConvention.Cdecl
+    }
+
+    let _get_initial_narrow_environment(sandbox: ISandbox) = {
+        ReturnValue = None
+        Convention = CallingConvention.Cdecl
+    }
