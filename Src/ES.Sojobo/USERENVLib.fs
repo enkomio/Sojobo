1016dab018f6b8bc058c8b63104612e917623621
Antonio Parata <aparata@gmail.com>
2019-05-16 15:16:00 +0200

    Added LoadUserProfileW (ExternFunctionType) from USERENV.dll

---
+++ Src/ES.Sojobo/USERENVLib.fs
@@ -0,0 +1,16 @@
+﻿namespace ES.Sojobo.Lib
+
+open System
+open ES.Sojobo
+open ES.Sojobo.Model
+
+module USERENV =
+    let loadUserProfileA(sandbox: ISandbox, hToken: UInt32, lpProfileInfo: UInt32) = {
+        ReturnValue = Some <| createInt32(0).Value
+        Convention = CallingConvention.Cdecl
+    }
+
+    let loadUserProfileW(sandbox: ISandbox, hToken: UInt32, lpProfileInfo: UInt32) = {
+        ReturnValue = Some <| createInt32(0).Value
+        Convention = CallingConvention.Cdecl
+    }
