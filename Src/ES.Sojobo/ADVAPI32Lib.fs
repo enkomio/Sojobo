7e4eb1e3bf26535118dcdc8c7fcd1e11f286bbf4
Antonio Parata <aparata@gmail.com>
2019-05-16 15:09:42 +0200

    Added function openProcessToken

---
+++ Src/ES.Sojobo/ADVAPI32Lib.fs
@@ -0,0 +1,11 @@
+﻿namespace ES.Sojobo.Lib
+
+open System
+open ES.Sojobo
+open ES.Sojobo.Model
+
+module ADVAPI32 =
+    let openProcessToken(sandbox: ISandbox, processHandle: UInt32, desiredAccess: UInt32, tokenHandle: UInt32) = {
+        ReturnValue = Some <| createInt32(0).Value
+        Convention = CallingConvention.Cdecl
+    }
