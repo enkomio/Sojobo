19a6e441ab2709c8a8d38bdd807b4a132147b33c
Antonio Parata <aparata@gmail.com>
2019-05-28 15:25:04 +0200

    Added feature  to allod a better module loading

--- Src/ES.Sojobo/Win32Sandbox.fs
+++ Src/ES.Sojobo/Win32Sandbox.fs
@@ -1,12 +1,18 @@
 ﻿namespace ES.Sojobo
 
 open System
+open System.IO
+open System.Collections.Generic
 open System.Reflection
 open B2R2
 open B2R2.FrontEnd.Intel
 open ES.Sojobo.Win32
 open ES.Sojobo.Model
+open System.Reflection.PortableExecutable
+open B2R2.FrontEnd
+open B2R2.BinFile.PE
 
+[<CLIMutable>]
 type Win32SandboxSettings = {
     /// This settings will initialize the environment by loading
     /// missing libraries or setup default hook for emulated functions
@@ -19,7 +25,8 @@ type Win32SandboxSettings = {
 type Win32Sandbox(settings: Win32SandboxSettings) as this =
     inherit BaseSandbox()
 
-    let mutable _stopExecution = false
+    let _hooks = new Dictionary<UInt64, Action<ISandbox>>()
+    let mutable _stopExecution: Boolean option = None
     let mutable _currentProcess: Win32ProcessContainer option = None
     do this.Emulator <- Some(upcast new LowUIREmulator(this))
 
@@ -35,36 +42,67 @@ type Win32Sandbox(settings: Win32SandboxSettings) as this =
         |> List.iter(this.GetRunningProcess().SetRegister)
 
     let resolveEmulatedFunctions() =
-        this.Libraries
-        |> Seq.iter(fun lib ->
-            match lib with
-            | Managed lib -> lib.ResolveLibraryFunctions()
-            | _ -> ()
+        getManagedLibraries(this.Libraries)
+        |> Seq.iter(fun lib -> lib.ResolveLibraryFunctions())
+
+    let getAllExportedFunctions() =
+        getNativeLibraries(this.Libraries)
+        |> Array.filter(fun lib -> lib.Filename.IsSome)
+        |> Array.collect(fun lib ->
+            lib.Exports
+            |> Seq.map(fun kv ->
+                let keyName = Utility.getFunctionKeyName(kv.Value, lib.Filename.Value |> Path.GetFileName)
+                (keyName, kv.Key)
+            )
+            |> Seq.toArray
         )
+        |> dict
 
     let mapEmulatedFunctions() =
-        this.Libraries
+        let exportedFunctions = getAllExportedFunctions()
+        getManagedLibraries(this.Libraries)
         |> Seq.iter(fun lib ->
-            match lib with
-            | Managed lib -> 
-                let proc = this.GetRunningProcess()
-                lib.MapSymbolWithManagedFunctions(proc.Memory, proc.GetImportedFunctions())
-            | _ -> ()
+            let proc = this.GetRunningProcess()
+            lib.MapSymbolWithManagedFunctions(proc.Memory, proc.GetImportedFunctions(), exportedFunctions)
         )
 
     let mapNativeLibraries() =
-        this.Libraries
+        getNativeLibraries(this.Libraries)
         |> Seq.iter(fun lib ->
-            match lib with
-            | Native lib -> 
-                try
-                    let assembly = Assembly.Load(lib.Content)
-                    this.AddLibrary(assembly)
-                with 
-                    | :? BadImageFormatException ->
-                        lib.Load(this.GetRunningProcess())
-            | _ -> ()
+            try
+                let assembly = Assembly.Load(lib.Content)
+                this.AddLibrary(assembly)
+            with 
+                | :? BadImageFormatException ->
+                    lib.Load(this.GetRunningProcess())
         )
+
+    let resolveHooks() =
+        this.Hooks
+        |> Seq.iter(function
+            | Address (addr, callback) ->
+                _hooks.[addr] <- callback
+            | Symbol (symbol, callback) ->
+                let items = symbol.ToLowerInvariant().Replace(".dll", String.Empty).Split([|'!'|])
+                let (moduleName, functionName) = (items.[0].Trim(), items.[1].Trim())
+                this.Libraries
+                |> Seq.iter(function 
+                    | Native lib when lib.Filename.IsSome -> 
+                        let filename = (Path.GetFileName <| lib.Filename.Value.ToLowerInvariant()).Replace(".dll", String.Empty).Trim()
+                        if moduleName.Equals(filename, StringComparison.OrdinalIgnoreCase) then
+                            // try to identify an exported function with the same name
+                            lib.Exports
+                            |> Seq.iter(fun kv ->
+                                if kv.Value.Equals(functionName, StringComparison.OrdinalIgnoreCase) then
+                                    _hooks.[kv.Key] <- callback
+                            )
+                    | _ -> ()
+                )
+        )   
+        
+    let mapManagedLibraries() =
+        resolveEmulatedFunctions()
+        mapEmulatedFunctions()
 
     let prepareForExecution() =
         if settings.InitializeEnvironment then
@@ -74,8 +112,10 @@ type Win32Sandbox(settings: Win32SandboxSettings) as this =
         mapNativeLibraries()
 
         // setup the emulated functions
-        resolveEmulatedFunctions()
-        mapEmulatedFunctions()
+        mapManagedLibraries()
+
+        // setup hooks
+        resolveHooks()
 
         // now that all libraries are mapped setup TEB and PEB
         setupTeb()
@@ -89,49 +129,87 @@ type Win32Sandbox(settings: Win32SandboxSettings) as this =
             | _ -> false
         )
 
-    let emulateNextInstruction(proc: BaseProcessContainer, endAddress: UInt64) =
+    let invokeRegisteredHook(programCounter: UInt64) =        
+        if _hooks.ContainsKey(programCounter) 
+        then _hooks.[programCounter].Invoke(this)
+
+    let emulateNextInstruction(proc: BaseProcessContainer, programCounter: UInt64) =
         let instruction = proc.ReadNextInstruction()
         let handler = proc.GetActiveMemoryRegion().Handler
+
+        invokeRegisteredHook(programCounter)
+
         this.Emulator.Value.EmulateInstruction(handler, instruction)
 
-        // check ending condition
-        _stopExecution <- 
-            _stopExecution || 
-            proc.GetProgramCounter().Value |> BitVector.toUInt64 >= endAddress
+    let rec loadLibraryFile(filename: String, loadedLibraries: HashSet<String>) =
+        let libPath = Environment.GetFolderPath(Environment.SpecialFolder.SystemX86)
+        let libName = Path.Combine(libPath, filename)
+        if File.Exists(libName) && loadedLibraries.Add(libName.ToLowerInvariant()) then
+            this.AddLibrary(libName)
+
+            // load also all referenced DLL
+            let handler = BinHandler.Init(ISA.OfString "x86", libName)
+            Utility.getPe(handler).ImportMap 
+            |> Seq.map(fun kv -> 
+                match kv.Value with
+                | ImportByOrdinal (_, dllname) -> dllname
+                | ImportByName (_, _, dllname) -> dllname
+            )
+            |> Seq.iter(fun dllName -> loadLibraryFile(dllName, loadedLibraries))
+
+    let loadReferencedLibraries() =
+        if settings.InitializeEnvironment then
+            let loadedLibraries = new HashSet<String>()
+            _currentProcess.Value.GetImportedFunctions()
+            |> Seq.distinctBy(fun symbol -> symbol.LibraryName)
+            |> Seq.map(fun lib -> lib.LibraryName)
+            |> Seq.iter(fun libName -> loadLibraryFile(libName, loadedLibraries))
             
-    new() = new Win32Sandbox(Win32SandboxSettings.Default)
+    new() = new Win32Sandbox(Win32SandboxSettings.Default)   
+    
+    override this.AddLibrary(filename: String) =
+        base.AddLibrary(filename)
+        match _stopExecution with
+        | Some _ ->
+            // process is running, map the library in memory too
+            let library = 
+                getNativeLibraries(this.Libraries) 
+                |> Seq.find(fun lib -> lib.Filename.Value.Equals(filename, StringComparison.Ordinal))
+            
+            // load native library
+            library.Load(this.GetRunningProcess())
+            
+            // map emulated function too            
+            mapManagedLibraries()
+        | _ -> ()
 
-    default this.AddHook(address: UInt64, callback: Action<ISandbox>) =
-        ()
-
-    default this.AddHook(symbol: String, callback: Action<ISandbox>) =
-        ()
-
-    default this.Run() =            
-        let win32Process = _currentProcess.Value
-        let activeRegion = win32Process.GetActiveMemoryRegion()
-        let endAddress = activeRegion.BaseAddress + uint64 activeRegion.Content.Length
-        
+    default this.Run() =
         // initialize structures and hooks    
         prepareForExecution()
                         
         // start execution loop
-        _stopExecution <- false
-        while not _stopExecution do
-            match tryGetEmulationLibrary(win32Process) with
-            | Some (Managed library) -> library.InvokeLibraryFunction(this)
-            | _ -> emulateNextInstruction(win32Process, endAddress)
+        _stopExecution <- Some false
+        while not _stopExecution.Value do
+            let programCounter = _currentProcess.Value.GetProgramCounter().Value |> BitVector.toUInt64
+            match tryGetEmulationLibrary(_currentProcess.Value) with
+            | Some (Managed library) -> 
+                invokeRegisteredHook(programCounter)
+                library.InvokeLibraryFunction(this)
+            | _ -> 
+                emulateNextInstruction(_currentProcess.Value, programCounter)
 
     default this.Stop() =
-        _stopExecution <- true
+        _stopExecution <- Some true
         
     default this.Load(filename: String) =
         _currentProcess <- new Win32ProcessContainer() |> Some
         _currentProcess.Value.Initialize(filename)
+        loadReferencedLibraries()
 
     default this.Load(buffer: Byte array) =
         _currentProcess <- new Win32ProcessContainer() |> Some
         _currentProcess.Value.Initialize(buffer)
+        loadReferencedLibraries()
 
     default this.GetRunningProcess() =
         _currentProcess.Value :> IProcessContainer
