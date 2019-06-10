591b3e6c245af7e7f1060602631928b26adf11d4
Antonio Parata <aparata@gmail.com>
2019-06-10 16:06:45 +0200

    Removed dead code from example

--- Src/EndToEndTests/SnapshotTests.fs
+++ Src/EndToEndTests/SnapshotTests.fs
@@ -1,8 +1,5 @@
 ﻿namespace ES.EndToEndTests
 
-open System
-open System.IO
-open System.Collections.Generic
 open B2R2
 open ES.Sojobo.Model
 open ES.Sojobo
@@ -20,12 +17,6 @@ module SnapshotTests =
         let proc = sandbox.GetRunningProcess()
         proc.Step.Add(fun proc ->
             ES.EndToEndTests.Utility.writeDisassembly(proc)
-            //ES.EndToEndTests.Utility.writeIR(proc)
-
-            (*
-            if Utility.isInRange(proc, 0x19145DUL, 0x191470UL) then
-                Utility.debuggerBreak(proc)
-            *)
 
             if 0x401061 = (proc.ProgramCounter.Value |> BitVector.toInt32) then
                 // just finished to compute fibonacci
