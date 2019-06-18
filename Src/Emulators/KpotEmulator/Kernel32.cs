e5db9d51bdc6784513081e7cfc759d23d18d1665
Antonio Parata <aparata@gmail.com>
2019-06-18 14:15:35 +0200

    Added emulators

---
+++ Src/Emulators/KpotEmulator/Kernel32.cs
@@ -0,0 +1,15 @@
+﻿using ES.Sojobo;
+using ES.Sojobo.CSharp;
+using static ES.Sojobo.Model;
+
+namespace KpotEmulator
+{
+    public static class Kernel32
+    {
+        public static CallbackResult GetLastError(ISandbox sandbox)
+        {
+            var functionReturnValue = BitVectorFactory.Create(0x57);
+            return new CallbackResult(functionReturnValue.ToOption(), CallingConvention.Cdecl);
+        }
+    }
+}
