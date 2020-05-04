54f07ee6e844bc0991b9d5cdda3da13d4dc5ab76
Antonio Parata <aparata@gmail.com>
2020-05-04 13:05:41 +0200

    Renamed files

---
+++ Src/ES.Sojobo.Windows/WindowsProcessContainer.fs
@@ -0,0 +1,116 @@
+﻿namespace ES.Sojobo.Windows
+
+open System
+open System.Collections.Generic
+open B2R2
+open B2R2.FrontEnd
+open B2R2.BinFile
+open ES.Sojobo
+open ES.Sojobo.Model
+open B2R2.FrontEnd.Intel
+
+type WindowsProcessContainer(pointerSize: Int32) as this =  
+    inherit BaseProcessContainer(pointerSize)
+    let _handles = new List<Handle>()    
+    let _iat = new List<Symbol>() 
+    let mutable _lastInstruction: Instruction option = None   
+
+    let setEntryPoint(handler: BinHandler) =
+        this.UpdateActiveMemoryRegion(this.Memory.GetMemoryRegion(handler.FileInfo.EntryPoint.Value))
+
+        let instructionPointer =
+            if pointerSize = 32 then        
+                createVariableWithValue(string Register.EIP, EmulatedType.DoubleWord, BitVector.ofUInt64 handler.FileInfo.EntryPoint.Value 32<rt>)
+            else
+                createVariableWithValue(string Register.RIP, EmulatedType.QuadWord, BitVector.ofUInt64 handler.FileInfo.EntryPoint.Value 64<rt>)
+
+        this.Cpu.SetRegister(instructionPointer)
+
+    let setupStackRegisters() =
+        let startAddress = this.Memory.Stack.BaseAddress + uint64(this.Memory.Stack.Content.Length - 8)
+        let topStackPointer = 
+            if pointerSize = 32 then               
+                createVariableWithValue(string Register.ESP, EmulatedType.DoubleWord, BitVector.ofUInt64 startAddress 32<rt>)
+            else
+                createVariableWithValue(string Register.RSP, EmulatedType.QuadWord, BitVector.ofUInt64 startAddress 64<rt>)
+        this.Cpu.SetRegister(topStackPointer)
+
+        let baseStackPointer = 
+            if pointerSize = 32 then  
+                createVariableWithValue(string Register.EBP, EmulatedType.DoubleWord, topStackPointer.Value)
+            else
+                createVariableWithValue(string Register.RBP, EmulatedType.QuadWord, topStackPointer.Value)
+        this.Cpu.SetRegister(baseStackPointer)
+        
+    let resolveIATSymbols(handler: BinHandler) =
+        handler.FileInfo.GetSymbols()
+        |> Seq.iter(fun symbol ->
+            if 
+                not(String.IsNullOrEmpty(symbol.LibraryName)) && 
+                (symbol.Kind = SymbolKind.ExternFunctionType || symbol.Kind = SymbolKind.FunctionType) 
+            then 
+                this.SetSymbol(symbol)
+                _iat.Add(symbol)
+        )
+
+    member internal this.Initialize(handler: BinHandler) =
+        Utility32.mapPe(handler, this.Memory)
+        setupStackRegisters()
+        setEntryPoint(handler)
+        resolveIATSymbols(handler)
+
+    member this.Initialize(buffer: Byte array) =
+        let handler = BinHandler.Init(ISA.DefaultISA, ArchOperationMode.NoMode, true, Addr.MinValue, buffer)
+        this.Initialize(handler)
+
+    member this.Initialize(filename: String) =  
+        this.SetFileName(filename)
+        let handler = BinHandler.Init(ISA.DefaultISA, ArchOperationMode.NoMode, true, Addr.MinValue, filename)        
+        this.Initialize(handler)
+
+    default this.GetImportedFunctions() =
+        Seq.readonly _iat
+
+    default this.Handles = _handles |> Seq.toArray   
+    
+    member this.AddHandle(handle: Handle) =
+        _handles.Add(handle)
+
+    default this.GetInstruction(address: UInt64) =
+        match _lastInstruction with
+        | Some instruction when address = instruction.Address -> instruction
+        | _ ->
+            try
+                _lastInstruction <- BinHandler.ParseInstr (this.GetActiveMemoryRegion().Handler) address |> Some
+                _lastInstruction.Value
+            with 
+                :? IndexOutOfRangeException ->
+                    // maybe the address is in another region, try to resolve the address
+                    let memRegion = this.Memory.GetMemoryRegion(address)
+                    BinHandler.ParseInstr memRegion.Handler address
+
+    default this.GetInstruction() =
+        this.GetInstruction(this.ProgramCounter.Value |> BitVector.toUInt64)
+
+    default this.ProgramCounter
+        with get() = 
+            if this.PointerSize = 32 then this.Cpu.GetRegister(Register.EIP.ToString())
+            else this.Cpu.GetRegister(Register.RIP.ToString())
+
+    default this.GetCallStack() = [|
+        let mutable baseStack = 
+            (if this.PointerSize = 32 then this.Cpu.GetRegister(Register.EBP.ToString())
+            else this.Cpu.GetRegister(Register.RBP.ToString())).Value
+            |> BitVector.toUInt64
+
+        let increment = this.PointerSize / 8 |> uint64
+        let readMemory(addr: UInt64) =
+            if this.PointerSize = 32 then (this.Memory.ReadMemory<UInt32>(addr) |> uint64)
+            else this.Memory.ReadMemory<UInt64>(addr)
+
+        let mutable retValue = readMemory(baseStack + increment)
+        while retValue <> 0UL do
+            yield retValue
+            baseStack <- readMemory(baseStack) |> uint64
+            retValue <- readMemory(baseStack + increment)
+    |]
---
+++ Src/ES.Sojobo.Windows/WindowsStructures.fs
@@ -0,0 +1,148 @@
+﻿namespace ES.Sojobo.Windows
+
+open System
+open System.Runtime.InteropServices
+
+(*
+    The following structures follow the definition provided by MS.
+    As general rule, reserved fields are not valorized.
+    Serialization info:
+        - Class type (as record) are serialized as pointer to anothe memory region
+        - Add "Struct" attribute if the class must be serialized as struct and not as a pointer
+        - For array type always add the "MarshalAs" with "SizeConst" property in order to know how many items must be serialized
+*)
+[<AutoOpen>]
+module WindowsStructures =
+    
+    // https://docs.microsoft.com/en-us/windows/desktop/api/subauth/ns-subauth-_unicode_string
+    [<CLIMutable>]
+    [<Struct>]
+    [<StructLayout(LayoutKind.Sequential, Pack=1, CharSet=CharSet.Ansi)>]
+    type UNICODE_STRING<'T> = {
+        Length: UInt16
+        MaximumLength: UInt16
+        Buffer: 'T
+    }
+
+    [<CLIMutable>]
+    [<Struct>]
+    [<StructLayout(LayoutKind.Sequential, Pack=1, CharSet=CharSet.Ansi)>]
+    type ListEntry<'T> = {
+        mutable Forward: 'T
+        mutable Backward: 'T
+    }
+    
+    // https://www.aldeid.com/wiki/LDR_DATA_TABLE_ENTRY 
+    // https://docs.microsoft.com/en-us/windows/win32/api/winternl/ns-winternl-peb_ldr_data  
+    [<CLIMutable>]
+    [<ReferenceEquality>]
+    [<StructLayout(LayoutKind.Sequential, Pack=1, CharSet=CharSet.Ansi)>]
+    type LDR_DATA_TABLE_ENTRY<'T> = {
+        mutable InLoadOrderLinks: ListEntry<LDR_DATA_TABLE_ENTRY<'T>>
+        mutable InMemoryOrderLinks: ListEntry<LDR_DATA_TABLE_ENTRY<'T>>
+        mutable InInitializationOrderLinks: ListEntry<LDR_DATA_TABLE_ENTRY<'T>>
+        DllBase: 'T
+        EntryPoint: 'T
+        Reserved3: 'T
+        FullDllName: UNICODE_STRING<'T>
+        BaseDllName: UNICODE_STRING<'T>        
+        [<MarshalAs(UnmanagedType.ByValArray, SizeConst = 3)>]
+        Reserved5: 'T array        
+        Reserved6: 'T
+        TimeDateStamp: UInt32
+    }
+
+    // https://cheesehack.tistory.com/99
+    // https://www.aldeid.com/wiki/PEB_LDR_DATA
+    // https://docs.microsoft.com/en-us/windows/win32/api/winternl/ns-winternl-peb_ldr_data
+    // https://www.geoffchappell.com/studies/windows/win32/ntdll/structs/peb_ldr_data.htm
+    [<CLIMutable>]
+    [<ReferenceEquality>]
+    [<StructLayout(LayoutKind.Sequential, Pack=1, CharSet=CharSet.Ansi)>]
+    type PEB_LDR_DATA<'T> = {
+        Length: UInt32
+        Initialized: UInt32
+        SsHandle: 'T
+        mutable InLoadOrderLinks: ListEntry<LDR_DATA_TABLE_ENTRY<'T>>
+        mutable InMemoryOrderLinks: ListEntry<LDR_DATA_TABLE_ENTRY<'T>>
+        mutable InInitializationOrderLinks: ListEntry<LDR_DATA_TABLE_ENTRY<'T>>
+        EntryInProgress: UInt32
+        ShutdownInProgress: UInt32
+        ShutdownThreadId: UInt32
+    }
+
+    // https://docs.microsoft.com/en-us/windows/desktop/api/winternl/ns-winternl-peb
+    // https://www.nirsoft.net/kernel_struct/vista/PEB.html
+    // https://www.aldeid.com/wiki/PEB-Process-Environment-Block
+    [<CLIMutable>]
+    [<ReferenceEquality>]
+    [<StructLayout(LayoutKind.Sequential, Pack=1, CharSet=CharSet.Ansi)>]
+    type PEB<'T> = {
+        [<MarshalAs(UnmanagedType.ByValArray, SizeConst = 2)>]
+        Reserved1: Byte array
+        BeingDebugged: Byte
+        [<MarshalAs(UnmanagedType.ByValArray, SizeConst = 1)>]
+        Reserved2: Byte array
+        [<MarshalAs(UnmanagedType.ByValArray, SizeConst = 2)>]
+        Reserved3: 'T array
+        Ldr: PEB_LDR_DATA<'T>
+        ProcessParameters: 'T
+        SubSystemData: 'T
+        ProcessHeap: 'T
+        FastPebLock: 'T
+        AtlThunkSListPtr: 'T
+        Reserved5: 'T
+        Reserved6: UInt32
+        Reserved7: 'T
+        Reserved8: UInt32
+        AtlThunkSListPtr32: UInt32
+        [<MarshalAs(UnmanagedType.ByValArray, SizeConst = 45)>]
+        Reserved9: 'T array
+        [<MarshalAs(UnmanagedType.ByValArray, SizeConst = 96)>]
+        Reserved10: Byte array
+        PostProcessInitRoutine: 'T
+        [<MarshalAs(UnmanagedType.ByValArray, SizeConst = 128)>]
+        Reserved11: Byte array
+        [<MarshalAs(UnmanagedType.ByValArray, SizeConst = 1)>]
+        Reserved12: 'T array
+        SessionId: UInt32
+    }
+    
+    // https://docs.microsoft.com/en-us/windows/win32/api/winternl/ns-winternl-teb
+    // https://www.nirsoft.net/kernel_struct/vista/TEB.html
+    // https://en.wikipedia.org/wiki/Win32_Thread_Information_Block
+    [<CLIMutable>]
+    [<ReferenceEquality>]
+    [<StructLayout(LayoutKind.Sequential, Pack=1, CharSet=CharSet.Ansi)>]
+    type TEB<'T> = {
+        // TIB
+        ExceptionList: 'T
+        StackBase: 'T
+        StackLimit: 'T
+        SubSystemTib: 'T
+        Version: 'T
+        ArbitraryUserPointer: 'T
+        Self: 'T
+
+        // TEB
+        EnvironmentPointer: 'T
+        [<MarshalAs(UnmanagedType.ByValArray, SizeConst = 2)>]
+        ClientId: 'T array
+        ActiveRpcHandle: 'T
+        ThreadLocalStoragePointer: 'T
+        ProcessEnvironmentBlock: PEB<'T>
+        [<MarshalAs(UnmanagedType.ByValArray, SizeConst = 399)>]
+        Reserved2: 'T array
+        [<MarshalAs(UnmanagedType.ByValArray, SizeConst = 1952)>]
+        Reserved3: Byte array
+        [<MarshalAs(UnmanagedType.ByValArray, SizeConst = 64)>]
+        TlsSlots: 'T array        
+        [<MarshalAs(UnmanagedType.ByValArray, SizeConst = 8)>]
+        Reserved4: Byte array
+        [<MarshalAs(UnmanagedType.ByValArray, SizeConst = 26)>]
+        Reserved5: 'T array
+        ReservedForOle: 'T
+        [<MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)>]
+        Reserved6: 'T array
+        TlsExpansionSlots: 'T
+    }
