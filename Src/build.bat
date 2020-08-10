f72009eba391c7fe7301f351c8dce6d9ea55dc18
Antonio Parata <aparata@gmail.com>
2020-08-10 23:35:29 +0200

    Formatting

--- Src/build.bat
+++ Src/build.bat
@@ -7,7 +7,7 @@ dotnet tool install fake-cli --tool-path fake
 @rem paket.exe install
 paket.exe install
 if errorlevel 1 (
-  exit /b %errorlevel%
+	exit /b %errorlevel%
 )
 
 ".\fake\fake.exe" run build.fsx %*
